97a90a8507680772df066c1573dc6d80787ce540
rosborne132 <ozborne132@gmail.com>
2020-09-27 17:04:56 -0700

    Add cinemachine

--- TileVania/Assets/Scenes/Scripts/Player.cs
+++ TileVania/Assets/Scenes/Scripts/Player.cs
@@ -6,32 +6,68 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     // Config
+    [SerializeField] float climbSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
     [SerializeField] float runSpeed = 5f;
 
     // State
     bool isAlive = true;
 
     // Cached component references
-    Rigidbody2D myRigidBody;
     Animator myAnimator;
+    Collider2D myCollider2D;
+    Rigidbody2D myRigidBody;
+    float gravityScaleAtStart;
 
-    // Start is called before the first frame update
     void Start()
     {
-        myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCollider2D = GetComponent<Collider2D>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+        gravityScaleAtStart = myRigidBody.gravityScale;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        Run();
+        ClimbLadder();
         FlipSprite();
+        Jump();
+        Run();
     }
 
     private bool HasHorizontalSpeed()
     {
         return Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+    }
+
+    private bool HasVerticalSpeed()
+    {
+        return Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
+    }
+
+    private void ClimbLadder()
+    {
+        // Check if our player is touching a climbable object.
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            myRigidBody.gravityScale = gravityScaleAtStart;
+            myAnimator.SetBool("Climbing", false);
+            return;
+        }
+
+        // This is to prevent the player from falling down the climbable object.
+        myRigidBody.gravityScale = 0;
+
+        // If so then climb
+        float controlThrow = CrossPlatformInputManager.GetAxis("Vertical"); // Value is between -1 to +1
+        Vector2 climbVelocity = new Vector2(myRigidBody.velocity.x, controlThrow * climbSpeed);
+        myRigidBody.velocity = climbVelocity;
+
+        // Animate climbing
+        bool playerHasVerticalSpeed = HasVerticalSpeed();
+        myAnimator.SetBool("Climbing", playerHasVerticalSpeed);
     }
 
     private void Run()
@@ -52,6 +88,20 @@ public class Player : MonoBehaviour
         if (playerHasHorizontalSpeed)
         {
             transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
+
+        return;
+    }
+
+    private void Jump()
+    {
+        // Check if our player is touching the ground.
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
         }
     }
 }
